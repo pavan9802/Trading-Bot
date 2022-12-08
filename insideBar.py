04566f5bd42fb6e7f967a50296901d8c0758b0e8
Pavan Patel <platinumpavan123@gmail.com>
2022-12-07 20:44:03 -0500

    adjusted to buy stocks when markets are open

--- insideBar.py
+++ insideBar.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 import math
 import time
-import random
+
 import threading
 
 BASE_URL = "https://paper-api.alpaca.markets"
@@ -11,9 +11,6 @@ KEY_ID = "PK9GDJPUBRJVGQFYIDXW"
 SECRET_KEY = "x7O11GYCcuJfxfps2jxeOh9PWxGxeFWEczLA4Rkw"
 api = REST(key_id=KEY_ID, secret_key=SECRET_KEY,
            base_url="https://paper-api.alpaca.markets")
-
-
-SYMBOLS = ["BTCUSD", "ETHUSD"]
 
 
 def get_position(symbol):
@@ -26,6 +23,7 @@ def get_position(symbol):
 
 def get_pause():
     now = datetime.now()
+
     next_min = now.replace(second=0, microsecond=0) + timedelta(minutes=1)
     pause = math.ceil((next_min - now).seconds)
     print(f"Sleep for {pause}")
@@ -33,19 +31,18 @@ def get_pause():
 
 
 def get_insideBar(bars):
-    return bars.high[-2] > bars.high[-2] and bars.low[-1] > bars.low[-1]
+    return bars.high[-2] > bars.high[-2] and bars.low[-2] < bars.low[-1]
 
 
 def buy(ticker, bars):
     while True:
-        quotes = api.get_quotes("AAPL", "2021-06-08",
-                                "2021-06-08", limit=10).df
+        quotes = api.get_quotes(ticker, limit=10).df
         avg = quotes.ask.rolling(10).mean()
         avg = round(avg, 2)
         if (avg > bars.high[-1]):
             notionalQTY = float(api.get_account().buying_power) * 0.05
             api.submit_order(ticker, notional=notionalQTY,
-                             side='buy', time_in_force='gtc', trail_percent="2",
+                             side='buy', time_in_force='day', trail_percent="2",
                              stop_loss=dict(
                                  stop_price=str(bars.low[-2]),
                                  limit_price=str(bars.low[-2])
@@ -60,14 +57,16 @@ while True:
     for ticker in SYMBOLS:
         notionalQTY = float(api.get_account().buying_power) * 0.05
         # GET DATA
-        bars = api.get_crypto_bars("BTCUSD", TimeFrame.Minute).df
-        bars = bars[bars.exchange == 'CBSE']
+        bars = api.get_bars(ticker, TimeFrame.Minute).df
+        # bars = bars[bars.exchange == 'CBSE']
         should_buy = get_insideBar(bars)
         position = get_position(symbol=ticker)
         # CHECK POSITIONS
         if position == 0 and should_buy == True:
             # Start a thread
+            print("inside")
             t1 = threading.Thread(target=buy, args=(ticker, bars))
+            t1.start()
 
     time.sleep(get_pause())
     print("*"*20)
--- maCross.py
+++ maCross.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 import math
 import time
-import random
+
 
 BASE_URL = "https://paper-api.alpaca.markets"
 KEY_ID = "PK9GDJPUBRJVGQFYIDXW"
@@ -12,9 +12,6 @@ api = REST(key_id=KEY_ID, secret_key=SECRET_KEY,
            base_url="https://paper-api.alpaca.markets")
 
 
-SYMBOLS = ["BTCUSD", "ETHUSD"]
-# SYMBOLS = ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA", "JNJ", "V", "WMT", "JPM",
-#            "NVDA", "MA", "META", "BAC", "CSCO", "SHEL", "ORCL", "MCD", "UPS", "MS", "NFLX", "QCOM", "GS"]
 SMA_FAST = 12
 SMA_SLOW = 24
 
@@ -23,6 +20,7 @@ def get_position(symbol):
     positions = api.list_positions()
     for p in positions:
         if p.symbol == symbol:
+            print(p.qty)
             return float(p.qty)
     return 0
 
@@ -39,14 +37,6 @@ def get_pause():
 # Same as the function in the random version
 
 
-def get_position(symbol):
-    positions = api.list_positions()
-    for p in positions:
-        if p.symbol == symbol:
-            return float(p.qty)
-    return 0
-
-
 # Returns a series with the moving average
 def get_sma(series, periods):
     return series.rolling(periods).mean()
@@ -61,23 +51,53 @@ def get_signal(fast, slow):
 # Get up-to-date 1 minute data from Alpaca and add the moving averages
 
 
-def get_bars(symbol):
-    bars = api.get_crypto_bars(symbol, TimeFrame.Minute).df
-    bars = bars[bars.exchange == 'CBSE']
-    bars[f'sma_fast'] = get_sma(bars.close, SMA_FAST)
-    bars[f'sma_slow'] = get_sma(bars.close, SMA_SLOW)
+def get_bars(symbol, stock):
+    if stock:
+        bars = api.get_bars(symbol, TimeFrame.Minute).df
+        bars[f'sma_fast'] = get_sma(bars.close, SMA_FAST)
+        bars[f'sma_slow'] = get_sma(bars.close, SMA_SLOW)
+    else:
+        bars = api.get_crypto_bars(symbol, TimeFrame.Minute).df
+        bars = bars[bars.exchange == 'CBSE']
+        bars[f'sma_fast'] = get_sma(bars.close, SMA_FAST)
+        bars[f'sma_slow'] = get_sma(bars.close, SMA_SLOW)
     return bars
 
 
-while True:
+def buy_stocks(SYMBOLS):
     for ticker in SYMBOLS:
         notionalQTY = float(api.get_account().buying_power) * 0.05
         # GET DATA
-        bars = get_bars(symbol=ticker)
+        bars = get_bars(symbol=ticker, stock=True)
         # CHECK POSITIONS
         position = get_position(symbol=ticker)
+
         should_buy = get_signal(bars.sma_fast, bars.sma_slow)
         print(f"Position: {position} / Should Buy: {should_buy}")
+        if position == 0 and should_buy == True:
+            # WE BUY ONE BITCOIN
+            api.submit_order(ticker, notional=notionalQTY,
+                             side='buy', time_in_force='day')
+            print(f'Symbol: {ticker} / Side: BUY / Quantity: {notionalQTY}')
+        elif position > 0 and should_buy == False:
+            # WE SELL ONE BITCOIN
+            api.submit_order(ticker, qty=position,
+                             side='sell', time_in_force='day')
+            print(f'Symbol: {ticker} / Side: SELL / Quantity: {position}')
+        time.sleep(.01)
+        print()
+
+
+def buy_crypto(SYMBOLS):
+    for ticker in SYMBOLS:
+        notionalQTY = float(api.get_account().buying_power) * 0.05
+        # GET DATA
+        bars = get_bars(symbol=ticker, stock=False)
+        # CHECK POSITIONS
+        position = get_position(ticker)
+
+        should_buy = get_signal(bars.sma_fast, bars.sma_slow)
+        print(f"Position: {position} / Should Buy {ticker}: {should_buy}")
         if position == 0 and should_buy == True:
             # WE BUY ONE BITCOIN
             api.submit_order(ticker, notional=notionalQTY,
@@ -85,9 +105,29 @@ while True:
             print(f'Symbol: {ticker} / Side: BUY / Quantity: {notionalQTY}')
         elif position > 0 and should_buy == False:
             # WE SELL ONE BITCOIN
-            api.submit_order(ticker, notional=position,
+            api.submit_order(ticker, qty=position,
                              side='sell', time_in_force='gtc')
             print(f'Symbol: {ticker} / Side: SELL / Quantity: {position}')
+        time.sleep(.01)
+
+        print()
+
+
+while True:
+    now = datetime.now()
+
+    if float(now.strftime("%H")) == 15 and float(now.strftime("%M")) == 59:
+        SYMBOLS = ["BTCUSD", "ETHUSD"]
+        buy_crypto(SYMBOLS)
+
+    elif float(now.strftime("%H")) >= 16:
+        SYMBOLS = ["BTCUSD", "ETHUSD"]
+        buy_crypto(SYMBOLS)
+
+    else:
+        SYMBOLS = ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA", "JNJ", "V", "WMT", "JPM",
+                   "NVDA", "MA", "META", "BAC", "CSCO", "SHEL", "ORCL", "MCD", "UPS", "MS", "NFLX", "QCOM", "GS"]
+        buy_stocks(SYMBOLS)
 
     time.sleep(get_pause())
     print("*"*20)
